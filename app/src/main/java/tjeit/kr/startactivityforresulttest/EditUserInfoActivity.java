3a56c3a988ef88e2eabd120a01fdfb671f8c1122
HanulGye <kyehl@naver.com>
2018-12-29 12:42:19 +0900

    Initial commit

---
+++ app/src/main/java/tjeit/kr/startactivityforresulttest/EditUserInfoActivity.java
@@ -0,0 +1,46 @@
+package tjeit.kr.startactivityforresulttest;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditUserInfoActivity extends BaseActivity {
+
+    private android.widget.EditText nameEdt;
+    private android.widget.Button okBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_info);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        this.okBtn = (Button) findViewById(R.id.okBtn);
+        this.nameEdt = (EditText) findViewById(R.id.nameEdt);
+
+    }
+}
